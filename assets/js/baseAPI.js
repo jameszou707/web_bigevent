e88ac18ba0574df72ed15ddcdd931f4a34bf95d6
jameszou <oumind@qq.com>
2020-11-24 17:34:30 +0800

    '首页开发'

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,31 @@
+
 // 1.为 页面上 所有基于 jq 的 ajax 请求发送之前，对参数对象做处理
 $.ajaxPrefilter(function (ajaxOpt) {
   // a.拼接 基地址
   ajaxOpt.url = 'http://ajax.frontend.itheima.net' + ajaxOpt.url;
+
+  // b.为 所有 /my/ 请求 添加 token
+  if (ajaxOpt.url.indexOf('/my/') > -1) {
+    ajaxOpt.headers = {
+      Authorization: localStorage.getItem('token')
+    }
+  }
+
+  // c.为 所有的 ajax请求 统一 配置 complete 事件函数 --------------------------------
+  ajaxOpt.complete = function (res) {
+    //c1.判断 返回的数据是否在告诉我们 【没有登录】
+    if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+      //c2.木有登录，则：
+      //c2.1显示 需要重新登录 的消息，显示结束后，再执行 清空token和跳转操作
+      layer.msg(res.responseJSON.message, {
+        icon: 1,
+        time: 1500 //1.5 秒关闭（如果不配置，默认是3秒）
+      }, function () {
+        //c2.2.清空 token
+        localStorage.removeItem('token');
+        //c2.3.跳转到 login.html
+        location.href = "/login.html";
+      });
+    }
+  }
 });
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+//1.在 dom树 创建完后 开始 执行
+$(function () {
+  getUserInfo();
+  $('#btnLogout').on('click', logout);
+})
+
+
+//1.加载 用户信息的 函数 -----------------
+function getUserInfo() {
+  $.ajax({
+    url: '/my/userinfo',
+    method: 'get',
+    // headers: {
+    //   Authorization: localStorage.getItem('token')
+    // },
+    success(res) {
+      if (res.status != 0) return layui.layer.msg(res.message);
+      //渲染 用户信息头像
+      renderAvatar(res.data);
+    }
+  });
+}
+
+//2.渲染用户信息函数 ----------------------
+function renderAvatar(usrData) {
+  //a.先获取 用户名（昵称/登录名）
+  let usrName = usrData.nickname || usrData.username;
+  //b.设置给 welcome span标签
+  $('#welcome').html(usrName);
+
+  //c.渲染头像
+  if (usrData.user_pic != null) {
+    //c1.有图片头像
+    $('.layui-nav-img').attr('src', usrData.user_pic).show();
+    // 隐藏文字头像
+    $('.text-avatar').hide();
+  } else {
+    //c2.没有图片头像，使用文本头像
+    $('.layui-nav-img').hide();// 隐藏 图片头像
+    let firstChar = usrName[0].toUpperCase();//获取名字首字
+    $('.text-avatar').text(firstChar).show();//设置文字并显示
+  }
+}
+
+//3.退出按钮函数
+function logout() {
+  // a.弹出 确认框
+  layui.layer.confirm('您确定要退出这个宇宙无敌超级系统吗？', { icon: 3, title: '系统提示' }, function (index) {
+    // b.删除 localStorage 中的 token 值
+    localStorage.removeItem('token');
+    // c.跳转到 login.html
+    location.href = "/login.html";
+
+    layer.close(index);
+  });
+}
